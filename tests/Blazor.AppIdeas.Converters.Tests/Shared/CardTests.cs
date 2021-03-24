f9ab0186293206bc5dd31238fd0e0860da0482f4
Pedro Silva <DarthPedro@gmail.com>
2021-03-24 13:18:49 -0700

    Created Card component, and add cards to home page for each converter app.

---
+++ tests/Blazor.AppIdeas.Converters.Tests/Shared/CardTests.cs
@@ -0,0 +1,61 @@
+﻿using Blazor.AppIdeas.Converters.Shared;
+using Bunit;
+using Xunit;
+
+namespace Blazor.AppIdeas.Converters.Tests.Shared
+{
+    public class CardTests
+    {
+        [Fact]
+        public void InitialRender()
+        {
+            // arrange
+            using var ctx = new TestContext();
+
+            // act
+            var cut = ctx.RenderComponent<Card>();
+
+            // assert
+            var expectedMarkup =
+@"  <div class=""card m-2 app-card-size"">
+      <div class=""text-center mt-2"">
+        <span class=""oi oi-card-image "" aria-hidden=""true""></span>
+      </div>
+      <div class=""card-body"">
+        <p class=""card-text""></p>
+      </div>
+    </div>
+";
+            cut.MarkupMatches(expectedMarkup);
+        }
+
+        [Fact]
+        public void InitialRender_WithParameters()
+        {
+            // arrange
+            using var ctx = new TestContext();
+
+            // act
+            var cut = ctx.RenderComponent<Card>(parameters => parameters
+                .Add(p => p.Title, "My Title")
+                .Add(p => p.ImageCssName, "oi-home")
+                .Add(p => p.ChildContent, "My card description.")
+                .Add(p => p.NavigationLink, "/home"));
+
+            // assert
+            var expectedMarkup =
+@"  <div class=""card m-2 app-card-size"">
+      <div class=""text-center mt-2"">
+        <span class=""oi oi-card-image oi-home"" aria-hidden=""true""></span>
+      </div>
+      <div class=""card-body"">
+        <h4 class=""card-title"">My Title</h4>
+        <p class=""card-text"">My card description.</p>
+        <a href=""/home"" class=""stretched-link""></a>
+      </div>
+    </div>
+";
+            cut.MarkupMatches(expectedMarkup);
+        }
+    }
+}
