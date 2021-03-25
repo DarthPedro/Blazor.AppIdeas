fd20e77b33576d9d00f719c04ff0c7c581e2ed85
Pedro Silva <DarthPedro@gmail.com>
2021-03-25 09:02:42 -0700

    Created corresponding unit test project; added required test frameworks for test projects.

---
+++ tests/Blazor.AppIdeas.Beginner.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Blazor.AppIdeas.Beginner.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
